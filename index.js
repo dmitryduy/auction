652d8d3ff1c2a2034304c7add4fac3cf92b74cc4
dmitryduy <cdcvh14@gmail.com>
2021-12-03 12:02:04 +0300

    add posibility editing paintings

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const https = require( "https" );
+const https = require("https");
 const bodyParser = require('body-parser');
 
 const options = {
@@ -25,12 +25,50 @@ app.get('/', (req, res) => {
 });
 
 app.get('/painting/:id', (req, res) => {
-   res.render("painting", {painting: books.find(book => book.id === +req.params.id)}) ;
+    res.render("painting", {painting: books.find(book => book.id === +req.params.id)});
 });
 
 app.get('/auction-info', (req, res) => {
     res.json(auctionInfo);
 })
 
+app.put('/editing', (req, res) => {
+    const {id, title, author, country, price, photo, available, minStep, maxStep} = req.body;
+    if (Number.isNaN(+price) || +price <=0) {
+        res.status(400);
+        res.end('Неверно установлена цена');
+    }
+    if (available !== 'true' && available !== 'false') {
+        res.status(400);
+        res.end('Поле "В аукциое?" должно принимать одно из двух значений: "true", "false"');
+    }
+    if (!title || !author || !country || ! available || !photo || !price || !minStep || !maxStep) {
+        res.status(400);
+        res.end('Есть незаполненные поля');
+    }
+    if (Number.isNaN(+minStep) || Number.isNaN(+maxStep)) {
+        res.status(404);
+        res.end(`Неверны введены значения минимального и максимального шагов`);
+    }
+    if (+minStep >= +maxStep) {
+        res.status(404);
+        res.end(`Минимальный шаг должен быть меньше максимального шага`);
+    }
+    const painting = books.find(painting => painting.id === +id);
+    if (!painting) {
+        res.status(404);
+        res.end(`Такая картина не найдена`);
+    }
+    painting.title = title;
+    painting.author = author;
+    painting.country = country;
+    painting.startPrice = price;
+    painting.image = photo;
+    painting.available = available;
+    painting.minStep = minStep;
+    painting.maxStep = maxStep;
+    res.status(200)
+    res.send("success");
+})
 
 
--- src/js/painting.js
+++ src/js/painting.js
@@ -12,4 +12,54 @@ $(document).ready(() => {
         }, 1000);
 
     });
+
+    let endTimer = false;
+
+    $('.editing__button').click(() => {
+        if (!endTimer) {
+            endTimer = true;
+            setTimeout(() => endTimer = false, 5000);
+            const id = $('.painting-content').attr('id');
+            const title = $('.editing__name').text();
+            const author = $('.editing__author').text();
+            const country = $('.editing__country').text();
+            const price = $('.editing__price').text();
+            const photo = $('.editing__photo').text();
+            const available = $('.editing__available').text();
+            const minStep = $('.editing__min-step').text();
+            const maxStep = $('.editing__max-step').text();
+            $.ajax({
+                url: '/editing',
+                type: 'PUT',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    id, title, author, country, price, photo, available, minStep, maxStep
+                })
+            })
+                .done(function() {
+                    $('.header__author').text(author);
+                    $('.header__title').text(title);
+                    $('.header__country').text(country);
+                    $('.image')
+                        .removeClass('not-available').removeClass('available')
+                        .addClass(`${available==='true'? 'available': 'not-available'}`)
+                        .attr('data-available',`${available==='true'? 'В аукционе': 'Не в аукционе'}`);
+                    $('.main__image').attr({'src':photo, 'alt': title});
+                    $('.bid__price').text(`$ ${price}`);
+                    $('.min-bid').text(`Мин. шаг: $${minStep}`);
+                    $('.max-bid').text(`Макс. шаг: $${maxStep}`);
+                    $('.success-popup').addClass('show').text('Данные изменены');
+                    setTimeout(() => $('.success-popup').removeClass('show'), 5000);
+                })
+                .fail(function(e) {
+                    $('.success-popup').addClass('show-incorrect').text(e.responseText);
+                    setTimeout(() => $('.success-popup').removeClass('show-incorrect'), 5000);
+                })
+        }
+        else {
+            $('.success-popup').addClass('show-incorrect').text('Нельзя отпралять запросы слишком часто');
+            setTimeout(() => $('.success-popup').removeClass('show-incorrect'), 5000);
+        }
+
+    })
 })
